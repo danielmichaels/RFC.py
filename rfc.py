c28ad973c2f07e3a8bc479f60d3ac7e9c0117e7d
Dan <dans.address@outlook.com>
2018-07-07 21:22:13 +0000

    Include function to parse text and write to database
    notes:
    + write_to_db: takes the contents of each rfc.txt writes to db
    + strip_extensions: util fn to remove unwanted items prior to db writing
    + global variables added - URL, FILENAME, STORAGE_PATH - as they are used
    by several functions.

--- rfc.py
+++ rfc.py
@@ -2,31 +2,26 @@
 import time
 
 import logging
-import requests
-from bs4 import BeautifulSoup
+import os
 from peewee import *
-from requests import ConnectionError
-from requests_futures.sessions import FuturesSession
 
 from models import Data, db
-from utils import download_rfc_tar, random_header, get_title, get_text, \
-    get_categories
 
 logging.basicConfig(level=logging.INFO)
 
 
+class Config:
+    STORAGE_PATH = 'test_rfc/'
+    URL = "https://www.rfc-editor.org/in-notes/tar/RFC-all.tar.gz"
+    FILENAME = URL.split('/')[-1]
+
+
 def main():
     start = time.time()
-    download_rfc_tar()
     try:
-        total_rfc = get_rfc_total()
-        iterate_over_rfcs(total_rfc)
-        # iterate_over_rfcs(total_rfc=55)  # manual debug only
+        write_to_db()
 
     except OSError:
-        raise
-
-    except ConnectionError:
         raise
 
     except KeyboardInterrupt:
@@ -37,68 +32,48 @@ def main():
         logging.info(f'This took: {end - start} to run!')
 
 
+def strip_extensions():
+    """Strips away all non txt files from directory listing.
+
+    :return clean_list: generator of files with unwanted items removed
+                        note: time for listcomp = 45s v genexp = 24s
+    """
+
+    _, _, files = next(os.walk('test_rfc/'))
+    print(type(files))
+    dirty_extensions = ['a.txt', 'rfc-index.txt', '.pdf', '.ps', '.ta']
+    clean_list = (x for x in files
+                  if not any(xs in x for xs in dirty_extensions))
+    print(type(clean_list))
+    return clean_list
 
 
-def iterate_over_rfcs(total_rfc):
-    """Iterate over the RFC list, checking to see if any RFC files already
-    exist in the folder and if not download a copy into the specified folder.
+def write_to_db():
+    """Write the contents of files to sqlite database."""
 
-    :arg takes in the total number of RFC's as the max_length in range()."""
+    for file in strip_extensions():
+        with open(os.path.join(Config.STORAGE_PATH, file), encoding='utf-8',
+                  errors='ignore') as f:
+            f = f.read()
 
-    session = FuturesSession(max_workers=10)
-    for num in range(1, total_rfc):
-        # for num in range(50, total_rfc):
-        url = f"https://tools.ietf.org/html/rfc{num}"
-        # check if rfc already exists in db
-        future = session.get(url, headers=random_header())
-        resp = future.result()
-        if resp.status_code == 200:
-            text = resp.text
-            logging.info(f'RFC{num} inserted')
-            write_to_db(num, text)
-        else:
-            print(f"RFC {num:04d} Exists.. Skipping..")
+            try:
+                number = file.strip('.txt').strip('rfc').strip('.pdf')
+                title = ''
+                body = f.strip()
+                category = ''
+                bookmark = False
 
-    else:
-        print(f"RFC {num:04d} DOES NOT EXIST")
+                with db.atomic():
+                    Data.create(number=number, title=title, text=body,
+                                category=category,
+                                bookmark=bookmark)
 
-
-def write_to_db(num, text):
-    try:
-        number = int(num)
-        title = get_title(text)
-        body = get_text(text)
-        category = get_categories(text)
-        bookmark = False
-
-        with db.atomic():
-            Data.create(number=number, title=title, text=body,
-                        category=category,
-                        bookmark=bookmark)
-
-    except IntegrityError as e:
-        logging.error(f'Integrity Error: {e} Raised!')
-    except AttributeError as e:
-        # maybe needs a time out takes ~ 5-7s + before raising error (rfc51)
-        logging.error(f'{e} hit at RFC {num}')
-        pass
-
-
-def get_rfc_total():
-    """Reach out to rfc-editor.org and scrape the table to get the total
-    number of RFC's available for download. This is used as the max value in
-    :func: iterate_over_rfcs
-
-    :returns total number of RFC's according to RFC tabular data."""
-
-    url = 'https://www.rfc-editor.org/rfc-index.html'
-    resp = requests.get(url)
-    text = resp.text
-    soup = BeautifulSoup(text, 'lxml')
-    tables = soup.find_all('table')
-    rfc_table = tables[2].find_all('tr')
-    logging.info(f'Number of RFC\'s available for download: {len(rfc_table)}')
-    return len(rfc_table)
+            except IntegrityError as e:
+                logging.error(f'Integrity Error: {e} Raised!')
+                pass
+            except AttributeError or ValueError as e:
+                logging.error(f'{e}: hit at RFC {file}')
+                pass
 
 
 if __name__ == '__main__':
