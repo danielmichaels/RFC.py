729b1eb0b72fd5ac761d92fa8a1bf65180b13ee8
Dan <dans.address@outlook.com>
2018-07-16 06:59:11 +0000

    Include homepage loop and search by rfc number db query lookup
    notes:
    + concatenated printer.py and rfc.py
    + search_by_number returns correct rfc number via click's pager (less)
    but has no error checking

--- rfc.py
+++ rfc.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
+import sys
 import time
 
+import click
 import logging
 import os
 from peewee import *
@@ -19,8 +21,10 @@ def main():
         # update.download_rfc_tar()
         # update.uncompress_tar()
         # create_tables()
-        write_to_db()
+        # write_to_db()
         # test_headers()
+        home_page()
+
 
     except OSError:
         raise
@@ -72,6 +76,92 @@ def test_headers():
     with open('test_rfc/rfc-index.txt', 'r') as index:
         index = index.read().strip()
         pass  # here for manual testing
+
+
+def clear_screen():
+    os.system('clear')
+
+
+class Color:
+    HEADER = '\033[95m'
+    IMPORTANT = '\33[35m'
+    NOTICE = '\033[33m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    RED = '\033[91m'
+    END = '\033[0m'
+    UNDERLINE = '\033[4m'
+    LOGGING = '\33[34m'
+
+
+logo = Color.HEADER + """
+          ____  _____ ____               
+ |  _ \|  ___/ ___|  _ __  _   _ 
+ | |_) | |_ | |     | '_ \| | | |
+ |  _ <|  _|| |___ _| |_) | |_| |
+ |_| \_\_|   \____(_) .__/ \__, |
+                    |_|    |___/ 
+                    """ + Color.END
+prompt = "rfc.py ~# "
+
+
+def home_page():
+    clear_screen()
+    print(logo + """
+    [1] -- Search by Number
+    [2] -- Search by Keyword
+    [3] -- Search by Category
+    [4] -- Search through Bookmark
+    [99] Quit!
+    """)
+    choice = input(prompt)
+    # early testing model
+    if choice == '1':
+        # search num
+        search_by_number()
+    elif choice == '2':
+        # search keyword
+        pass
+    elif choice == '3':
+        # search category
+        pass
+    elif choice == '4':
+        # search bookmarks
+        pass
+    elif choice == '99':
+        sys.exit()
+    # else:
+    #     choice
+
+
+def search_by_number():
+    clear_screen()
+    number = input('enter RFC by number: >> ')
+    print('number:', number)
+    result = Data.get_by_id(number).text
+    # needs error checking index errors etc
+    pager(result)
+
+
+def search_by_keyword():
+    pass
+
+
+def search_by_category():
+    pass
+
+
+def bookmark_rfc():
+    pass
+
+
+def pager(data):
+    click.echo_via_pager(data)
+    input('Do you want to bookmark this? >> [y/N] ')
+    # input will need routing to bookmark in future.
+    # needs error checking such as 'isdigit' etc
+    home_page()
 
 
 if __name__ == '__main__':
