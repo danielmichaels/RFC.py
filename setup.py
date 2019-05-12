028a00564d51774f1d35590f5fe1ec954bee6024
danielmichaels <dans.address@outlook.com>
2019-05-12 21:58:46 +1000

    Update README in prep for release

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ $ twine upload -r pypi dist/*
 from setuptools import setup, find_packages
 
 NAME = "RFC.py"
-VERSION = "1.3"
+VERSION = "2019.5.1"
 DESCRIPTION = (
     "A simple python client that offers users the ability to search"
     " for, read and bookmark RFC's from the Internet Engineering "
